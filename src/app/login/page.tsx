d9bd2b38fea8e9b339bc000caab6b818ca07e062
yomaurice <74863761+yomaurice@users.noreply.github.com>
2025-08-29 05:52:02 +0300

    fix for vercel

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -64,7 +64,7 @@ export default function Login() {
           </button>
         </form>
         <p className="mt-4 text-sm text-center text-gray-600">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <a href="/signup" className="text-blue-600 hover:underline">
             Sign up here
           </a>
