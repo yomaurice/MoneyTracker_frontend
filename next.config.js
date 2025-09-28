168d314ed97d0fecbc63c670c2bc2b172c6ce46d
yomaurice <74863761+yomaurice@users.noreply.github.com>
2025-09-28 21:22:23 +0300

    remove rewrites

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL}/api/:path*`,
-      },
-    ];
-  },
+  
 };
 
 module.exports = nextConfig;
